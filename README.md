d0c304dd31dcfbf9d61a872a1f8641c8d74c16fa
Poptartkay <fordjulietkay@gmail.com>
2022-12-12 17:37:18 -0800

    added in time functions to JS as well as updated read me

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # octo-balancer
+
+## Table of contents
+​
+  - [The challenge](#the-challenge)
+  - [Links]
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+​
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+​
+​
+### The challenge
+​
+User Story - 
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+Acceptance Criteria -
+ GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+​
+
+​
+### Links
+​
+
+- Live Site URL:
+​
+### Built with
+​
+- HTML
+- CSS
+- JavaScript
+
+​
+### What I learned
+
+
+​
+
+​
+### Continued development
+
+
+​
+## Author
+Kay Ford
+
--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+
+
+/**
+ * 
+ * @param {Date} date 
+ */
+
+function formatTime(date) {
+  const hours12 = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const isAM = date.getHours() < 12;
+
+  return '${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} ${isAM ? "AM" : "PM"}';
+
+}
+/**
+ * 
+ * @param {Date} date 
+ */
+
+function formatDate(date) {
+  const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  return '${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}';
+
+}
+
+setInterval(() => {
+  const now = new Date();
+
+  timeElement.textContent = formatTime(now);
+  dateElement.textContent = formatDate(now);
+}, 200);
+
+
 $(function () {
   let hour9 = $('#9');
   let hour10 = $('#10');
